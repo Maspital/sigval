6e3f7fa1924d6e6e353be785d6e58f63f1a19ba7
Philipp Schlipper <philipp.schlipper@fkie.fraunhofer.de>
2023-02-16 13:46:47 +0100

    Refactor coloring in print statements

--- src/validate_mapping.py
+++ src/validate_mapping.py
@@ -1,3 +1,8 @@
+WARN = "\033[93m[WARNING]"
+INFO = "\033[92m[INFO]"
+END = "\033[0m"
+
+
 def compare_mapping_to_logs(fields_mapped_to, fields_used_in_events):
     print("=" * 25 + " COMPARING MAPPED FIELDS TO FIELDS IN EVENTS " + "=" * 25)
     fields_without_usage = []
@@ -7,12 +12,12 @@ def compare_mapping_to_logs(fields_mapped_to, fields_used_in_events):
             fields_without_usage.append(field)
 
     if fields_without_usage:
-        print("\033[93m[WARNING] The following fields are mapped TO in your mapping, "
-              "but don't occur in any event:\033[0m")
+        print(f"{WARN} The following fields are mapped TO in your mapping, "
+              f"but don't occur in any event:{END}")
         print(fields_without_usage)
-        print(f"\033[93m[WARNING] Total amount of unused fields in mapping file: {len(fields_without_usage)}\033[0m\n")
+        print(f"{WARN} Total amount of unused fields in mapping file: {len(fields_without_usage)}{END}\n")
     else:
-        print("\033[92m[INFO] All fields mapped to occur in at least one event.\033[0m\n")
+        print(f"{INFO} All fields mapped to occur in at least one event.{END}\n")
 
 
 def compare_rules_to_mapping(fields_used_by_rules, fields_mapped_from):
@@ -24,12 +29,12 @@ def compare_rules_to_mapping(fields_used_by_rules, fields_mapped_from):
             fields_without_mapping.append(field)
 
     if fields_without_mapping:
-        print("\033[93m[WARNING] The following fields are used by a Sigma rule, but are not mapped FROM in your "
-              "mapping:\033[0m")
+        print(f"{WARN} The following fields are used by a Sigma rule, but are not mapped FROM in your "
+              f"mapping:{END}")
         print(fields_without_mapping)
-        print(f"\033[93m[WARNING] Total amount of unmapped fields in Sigma rules: {len(fields_without_mapping)}\033[0m\n")
+        print(f"{WARN} Total amount of unmapped fields in Sigma rules: {len(fields_without_mapping)}{END}\n")
     else:
-        print("\033[92m[INFO] All fields used by Sigma are present in the mapping.\033[0m\n")
+        print(f"{INFO} All fields used by Sigma are present in the mapping.{END}\n")
 
 
 def compare_mapping_to_rules(fields_mapped_from, fields_used_by_rules):
@@ -40,9 +45,9 @@ def compare_mapping_to_rules(fields_mapped_from, fields_used_by_rules):
             obsolete_mappings.append(field)
 
     if obsolete_mappings:
-        print("\033[93m[WARNING] The following fields appear in your mapping, but are not used by any "
-              "Sigma rule:\033[0m")
+        print(f"{WARN} The following fields appear in your mapping, but are not used by any "
+              f"Sigma rule:{END}")
         print(obsolete_mappings)
-        print(f"\033[93m[WARNING] Total amount of obsolete fields in mapping: {len(obsolete_mappings)}\033[0m\n")
+        print(f"{WARN} Total amount of obsolete fields in mapping: {len(obsolete_mappings)}{END}\n")
     else:
-        print("\033[92m[INFO] All fields present in the mapping are used by at least one Sigma rule.\033[0m\n")
+        print(f"{INFO} All fields present in the mapping are used by at least one Sigma rule.{END}\n")
