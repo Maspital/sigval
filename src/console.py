b754341ba7e0eaa72a7d49a19c5b26cf11f40287
Philipp Schlipper <philipp.schlipper@fkie.fraunhofer.de>
2023-03-09 15:01:20 +0100

    Add logo and version to output

--- src/console.py
+++ src/console.py
@@ -5,8 +5,8 @@ from file_handler import fields_from_rules, fields_from_logs, fields_from_mappin
 
 
 _global_options = [
-    click.option('--mapping-type', "-m", type=click.Choice(['chainsaw', 'logprep'], case_sensitive=False),
-                 default="chainsaw", help="Defines how sigval will parse the mapping file (default: chainsaw)"),
+    click.option('--mapping-type', "-m", type=click.Choice(['chainsaw', 'sigma'], case_sensitive=False),
+                 default="sigma", help="Defines how sigval will parse the mapping file (default: sigma)"),
     click.option("--diff-view", "-d", is_flag=True, default=False,
                  help="Format output such that it can be used for further processing, omit info and warnings."),
 ]
@@ -30,6 +30,7 @@ def global_options(func):
 @click.argument("winlogbeat_file")
 def mapping_to_logs(mapping_type, diff_view, mapping_file, winlogbeat_file):
     """Compare fields mapped *TO* to winlogbeat events"""
+    prelude(diff_view)
     fields_mapped_to = fields_from_mapping("to", mapping_file, mapping_type)
     fields_occurring_in_events = fields_from_logs(winlogbeat_file)
     compare_mapping_to_logs(fields_mapped_to, fields_occurring_in_events, diff_view)
@@ -41,6 +42,7 @@ def mapping_to_logs(mapping_type, diff_view, mapping_file, winlogbeat_file):
 @click.argument("rules_dir")
 def mapping_to_rules(mapping_type, diff_view, mapping_file, rules_dir):
     """Compare fields mapped *FROM* to Sigma rules"""
+    prelude(diff_view)
     fields_mapped_from = fields_from_mapping("from", mapping_file, mapping_type)
     fields_used_by_rules = fields_from_rules(rules_dir)
     compare_mapping_to_rules(fields_mapped_from, fields_used_by_rules, diff_view)
@@ -52,9 +54,23 @@ def mapping_to_rules(mapping_type, diff_view, mapping_file, rules_dir):
 @click.argument("rules_dir")
 def rules_to_mapping(mapping_type, diff_view, mapping_file, rules_dir):
     """Compare fields in Sigma rules to fields mapped *FROM*"""
+    prelude(diff_view)
     fields_mapped_from = fields_from_mapping("from", mapping_file, mapping_type)
     fields_used_by_rules = fields_from_rules(rules_dir)
     compare_rules_to_mapping(fields_used_by_rules, fields_mapped_from, diff_view)
+
+
+def prelude(diff_view):
+    logo = r"""
+       _____ _      _    __      __
+      / ___/(_)___ | |  / /___ _/ /
+      \__ \/ / __ `/ | / / __ `/ / 
+     ___/ / / /_/ /| |/ / /_/ / /  
+    /____/_/\__, / |___/\__,_/_/   
+           /____/  v1.0.0                    
+        """
+    if not diff_view:
+        print(logo)
 
 
 if __name__ == '__main__':
