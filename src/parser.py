aefec314eac4c47fe9cee448bdc71777c02505bb
Philipp Schlipper <s6pischl@uni-bonn.de>
2023-03-14 11:37:49 +0100

    Improve error handling when parsing files

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,4 @@
 import yaml
-from sys import exit
 
 
 def chainsaw_mapping(direction, mapping_file):
@@ -10,7 +9,11 @@ def chainsaw_mapping(direction, mapping_file):
     for group in mapping["groups"]:
         # there is usually only one group, but theoretically more can be defined
         for field in group["fields"]:
-            fields_mapped.append(field[direction])
+            if type(field[direction]) is str:
+                fields_mapped.append(field[direction])
+            else:
+                raise TypeError(f"Unexpected data type \"{type(field[direction])}\" for value \"{field[direction]}\" "
+                                f"when parsing chainsaw mapping file.")
     fields_mapped = list(set(fields_mapped))
     fields_mapped.sort()
     return fields_mapped
@@ -33,12 +36,13 @@ def sigma_mapping(direction, mapping_file):
             data_type = type(mapping[key])
             if data_type is str:
                 fields_mapped.append(mapping[key])
-            if data_type is dict:
+            elif data_type is dict:
                 fields_mapped.extend(list(mapping[key].values()))
-            if data_type is list:
+            elif data_type is list:
                 fields_mapped.extend(mapping[key])
             else:
-                print(f"\033[91m[WARNING]\033[0m Unexpected data type: {data_type}")
+                raise TypeError(f"Unexpected data type \"{data_type}\" for value \"{mapping[key]}\" "
+                                f"when parsing sigma mapping file.\n Expected str, dict or list.")
 
     fields_mapped = list(set(fields_mapped))
     fields_mapped.sort()
@@ -56,12 +60,13 @@ def get_all_keys(current_dict, previous_key):
         elif data_type in [str, int, list, bool]:
             all_keys.append(f"{previous_key}{key}")
         else:
-            print(f"\033[91m[WARNING]\033[0m Unexpected data type: {data_type}")
+            raise TypeError(f"Unexpected data type \"{data_type}\" for value \"{current_dict[key]}\" "
+                            f"when parsing log entries. Expected dict, str, int, list or bool.")
     return all_keys
 
 
 def get_fields_from_list(list_of_stuff):
-    # Strings and dict values are not desired, only get keys of dicts inside the list
+    # Strings and dict values within Sigma rule are not desired, only get keys of dicts inside the list
     contained_fields = []
     for entry in list_of_stuff:
         if type(entry) is str:
@@ -70,5 +75,6 @@ def get_fields_from_list(list_of_stuff):
             keys = entry.keys()
             contained_fields.extend(list(keys))
         else:
-            print(f"\033[91m[WARNING]\033[0m Unexpected data type: {type(entry)}")
+            raise TypeError(f"Unexpected data type \"{type(entry)}\" for value \"{entry}\" "
+                            f"when parsing sigma rules. Expected str or dict.")
     return contained_fields
